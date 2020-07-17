5f1a02a09871da6e47e9a80a8663d06c23bf3a57
johnnyurosevic <johnnyurosevic@g.ucla.edu>
2020-07-17 04:40:43 -0700

    Convert a frame_buffer to image and draw

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use ggez::graphics;
 use ggez::nalgebra as na;
 use ggez::{Context, GameResult};
 
-const SCREEN_WIDTH : f32 = 1280.0;
-const SCREEN_HEIGHT : f32 = 720.0;
+const SCREEN_WIDTH : u16 = 1280;
+const SCREEN_HEIGHT : u16 = 720;
+const NUM_PIXELS : usize = SCREEN_HEIGHT as usize * SCREEN_WIDTH as usize * 4;
 
 struct GameState {
     pos_x: f32,
@@ -25,17 +26,12 @@ impl event::EventHandler for GameState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+        let sky_blue : [u8; 4] = [135, 206, 235, 255];
+        let mut frame : Vec<u8> = sky_blue.iter().cloned().cycle().take(NUM_PIXELS).collect();
 
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            na::Point2::new(0.0, 0.0),
-            100.0,
-            2.0,
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(self.pos_x, 380.0),))?;
+        let image = graphics::Image::from_rgba8(ctx, SCREEN_WIDTH, SCREEN_HEIGHT, frame.as_slice())?;
+        
+        graphics::draw(ctx, &image, (na::Point2::new(0.0, 0.0),))?;
 
         graphics::present(ctx)?;
         Ok(())
@@ -45,7 +41,7 @@ impl event::EventHandler for GameState {
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("doom", "ggez")
     .window_setup(ggez::conf::WindowSetup::default().title("Shooter"))
-    .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_WIDTH, SCREEN_HEIGHT));
+    .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32));
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut GameState::new()?;
     event::run(ctx, event_loop, state)
