553b974d8188afd9d7ee498197fbded1fe5c71a9
johnnyurosevic <johnnyurosevic@g.ucla.edu>
2020-07-25 21:43:48 -0700

    Rays go the right direction and draw close walls

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use ggez::input::keyboard;
 use ggez::nalgebra as na;
 use ggez::{Context, GameResult};
 
-const SCREEN_WIDTH : usize = 720;
+const SCREEN_WIDTH : usize = 1024;
 const SCREEN_HEIGHT : usize = 720;
 const NUM_PIXELS : usize = SCREEN_HEIGHT * SCREEN_WIDTH * 4;
 
@@ -48,8 +48,8 @@ impl GameState {
             [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
         ];
         let s = GameState { 
-            pos: na::Point2::new(15.0, 12.0),
-            dir: 0.25,
+            pos: na::Point2::new(4.5, 21.5),
+            dir:  0.25,
             map,
         };
         Ok(s)
@@ -80,6 +80,8 @@ impl event::EventHandler for GameState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let sky_blue : [u8; 4] = [135, 206, 235, 255];
         let grey : [u8; 4] = [128, 128, 128, 255];
+        let green : [u8; 4] = [0, 255, 0, 255];
+
         let mut frame : Vec<u8> = sky_blue.iter().cloned().cycle().take(NUM_PIXELS).collect();
 
         let mut draw = |x: usize, y: usize, color: [u8; 4]| {
@@ -87,57 +89,73 @@ impl event::EventHandler for GameState {
             frame[index..(4 + index)].clone_from_slice(&color);
         };
 
-        let view_left = na::Vector2::new((self.dir + 0.1).cos(), (self.dir + 0.1).sin());
-        let view_right = na::Vector2::new((self.dir - 0.1).cos(), (self.dir - 0.1).sin());
+        let view_left = na::Vector2::new((self.dir + 0.5).cos(), (self.dir + 0.5).sin());
+        let view_right = na::Vector2::new((self.dir - 0.5).cos(), (self.dir - 0.5).sin());
 
         for x in 0..SCREEN_WIDTH {
-            let camera_x = x as f32 / SCREEN_WIDTH as f32;
-            let ray_dir = na::Vector2::new(view_left.x * (1.0 - camera_x), view_left.y * (1.0 - camera_x)) 
-                + na::Vector2::new(view_right.x * camera_x, view_right.y * camera_x);
-                
+            let t = x as f32 / SCREEN_WIDTH as f32;
+            let ray_dir = na::Vector2::new(view_left.x * (1.0 - t), view_left.y * (1.0 - t)) 
+                + na::Vector2::new(view_right.x * t, view_right.y * t);
+
             let skip = na::Vector2::new(1.0 / ray_dir.x.abs(), 1.0 / ray_dir.y.abs());
 
             let dist_x = if ray_dir.x > 0.0 {
-                self.pos.x.ceil() - self.pos.x
+                1.0-(self.pos.x % 1.0)
             }
             else {
-                self.pos.x - self.pos.x.floor()
+                self.pos.x % 1.0
             };
 
             let dist_y = if ray_dir.y > 0.0 {
-                self.pos.y.ceil() - self.pos.y
+                1.0-(self.pos.y % 1.0)
             }
             else {
-                self.pos.y - self.pos.y.floor()
+                self.pos.y % 1.0
             };
 
             let mut dist = na::Vector2::new(dist_x * skip.x, dist_y * skip.y);
-
+            
             let mut map = na::Vector2::new(self.pos.x as isize, self.pos.y as isize);
             let mut traveled = 0.0;
-
+            
             let mut hit = false;
-
+            
             while !hit {
                 if dist.x < dist.y {
-                    map.x += dist.x.signum() as isize;
+                    map.x += ray_dir.x.signum() as isize;
                     traveled += dist.x;
                     dist.y -= dist.x;
                     dist.x = skip.x;
                 }
                 else {
-                    map.y += dist.y.signum() as isize;
+                    map.y += ray_dir.y.signum() as isize;
                     traveled += dist.y;
                     dist.x -= dist.y;
                     dist.y = skip.y;
                 }
+                
+                if traveled as usize == 0 {
+                    traveled = 1.0;
+                }
 
                 if self.map[map.y as usize][map.x as usize] > 0 {
-                    let bottom = SCREEN_HEIGHT / 2 + SCREEN_HEIGHT / traveled as usize;
-                    let top = SCREEN_HEIGHT / 2 - SCREEN_HEIGHT / traveled as usize;
+                    let mut bottom = SCREEN_HEIGHT / 2 + SCREEN_HEIGHT / traveled as usize;
+                    
+                    if bottom >= SCREEN_HEIGHT {
+                        bottom = SCREEN_HEIGHT - 1;
+                    }
+
+                    let top_signed = SCREEN_HEIGHT as isize / 2 - SCREEN_HEIGHT as isize / traveled as isize;
+
+                    let top = std::cmp::max(top_signed, 0) as usize;
+                    
+                    let color = match self.map[map.y as usize][map.x as usize] {
+                        4 => green,
+                        _ => grey,
+                    };
 
                     for y in top..bottom + 1 {
-                        draw(x, y, grey);
+                        draw(x, y, color);
                     }
 
                     hit = true;
